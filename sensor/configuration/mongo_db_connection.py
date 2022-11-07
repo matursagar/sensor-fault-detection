a1e305631aee1c8d8f74a47089769299f30b0b28
matursagar <matursagar@gmail.com>
2022-11-07 11:48:03 +0530

    Updated mongo_db_connection.py file

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -10,8 +10,7 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                #mongo_db_url = os.getenv(MONGODB_URL_KEY)
-                mongo_db_url  = "mongodb+srv://matursagar:tanmaysag@cluster0.dsf7q06.mongodb.net/?retryWrites=true&w=majority"
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
